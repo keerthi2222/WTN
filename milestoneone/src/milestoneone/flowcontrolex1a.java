ec1d9cd45d6f4be68f365e3665d984f59e6bcfa6
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-12 13:54:24 +0530

    Flow control statements 1A

---
+++ milestoneone/src/milestoneone/flowcontrolex1a.java
@@ -0,0 +1,21 @@
+package milestoneone;
+
+public class flowcontrolex1a {
+	public static void main(String args[])
+	{
+		int n=30;
+		if(n>0)
+		{
+			System.out.println(n+" "+"positive");
+		}
+		else if(n<0)
+		{
+		System.out.println(n+" "+"Negative");
+		}
+		else
+		{
+			System.out.println(n+" " +"Zero");
+		}
+	}
+
+}
