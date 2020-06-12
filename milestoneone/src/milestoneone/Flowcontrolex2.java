f2aff04f85e335128a0efadf381e831817cc89b8
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-12 13:58:04 +0530

    Flow control statements 2

---
+++ milestoneone/src/milestoneone/Flowcontrolex2.java
@@ -0,0 +1,17 @@
+package milestoneone;
+
+public class Flowcontrolex2 {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+
+}
