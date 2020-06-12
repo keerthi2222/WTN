34edc39dcc0b6304bdb36c2d21aed6b556c2efc9
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-12 19:48:18 +0530

    Flow control statements 13

---
+++ milestoneone/src/milestoneone/Flowcontrolex13.java
@@ -0,0 +1,24 @@
+package milestoneone;
+
+public class Flowcontrolex13 {
+	public static void main(String args[])
+	{
+		int temp;
+		for(int i=10;i<=99;i++)
+		{
+			temp=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					temp=1;
+					break;
+				}
+			}
+			if(temp==0)
+				System.out.println(i);
+		}
+		
+	}
+
+}
