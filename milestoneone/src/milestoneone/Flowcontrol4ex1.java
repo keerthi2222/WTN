a4d365d5d660842309a94e92a5d9c4ef602db70b
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-12 15:59:53 +0530

    Flow control statements 4 Example1

---
+++ milestoneone/src/milestoneone/Flowcontrol4ex1.java
@@ -0,0 +1,16 @@
+package milestoneone;
+
+public class Flowcontrol4ex1 {
+	public static void main(String args[])
+	{
+		char item1='s';
+		char item2='e';
+		if(item1>item2)
+		{
+			System.out.println(item2+","+item1);
+		}
+		
+		
+	}
+
+}
