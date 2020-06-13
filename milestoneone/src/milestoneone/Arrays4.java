af58335f778348c7da8282528bfcdf75e590c0bf
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-13 11:48:16 +0530

    Arrays 4

---
+++ milestoneone/src/milestoneone/Arrays4.java
@@ -0,0 +1,15 @@
+package milestoneone;
+
+public class Arrays4 {
+	public static void main(String args[])
+	{
+		int num[]= {119,120,98,115};
+		String str=null;
+		for(int i:num)
+		{
+			str=Character.toString((char)i);
+			System.out.print( str);
+		}
+	}
+
+}
