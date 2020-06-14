4edc861c2a68295caeb48c6ec050462c9c2b4224
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-14 21:54:15 +0530

    Arrays 8

---
+++ milestoneone/src/milestoneone/Arrays8.java
@@ -0,0 +1,29 @@
+package milestoneone;
+
+public class Arrays8 
+{
+	public static void main(String args[])
+	{
+		int[] array= {1,6,7,9,4};
+		int sum=0;
+		int six=-1;
+		int seven=-1;
+		for(int i=0;i<array.length;i++)
+		{
+			if(array[i]==6)
+				six=i;
+			if(array[i]==7)
+				seven=i;
+		}
+		if(seven==-1)
+			six=-1;
+		for(int i=0;i<array.length;i++)
+		{
+			if(six!=-1&&i>=six&&i<=seven)
+				continue;
+			sum+=array[i];
+		}
+		System.out.println(sum);
+	}
+
+}
