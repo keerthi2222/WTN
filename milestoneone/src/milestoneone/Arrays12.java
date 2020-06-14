b1c5b3d62a549a5802e782a69ad0c9f8a61d5dd2
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-14 19:56:42 +0530

    Arrays 12

---
+++ milestoneone/src/milestoneone/Arrays12.java
@@ -0,0 +1,15 @@
+package milestoneone;
+
+public class Arrays12
+{
+	public static void main(String ags[])
+	{
+		int []a=new int[] {1,2,3};
+		int []b=new int[] {1,4,5};
+		int []c=new int[2];
+		c[0]=a[1];
+		c[1]=b[1];
+		System.out.print(c[0]+" "+c[1]);
+	}
+
+}
