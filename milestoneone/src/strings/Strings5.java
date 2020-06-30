c5e7df8e522717cd111e4e382d3393e4622bce54
lenovo <lenovo@DESKTOP-NN19MM0>
2020-06-30 09:18:43 +0530

    Strings 5

---
+++ milestoneone/src/strings/Strings5.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Strings5 
+{
+	public static void main(String args[])
+	{
+		String str1="Suman";
+		int len=str1.length();
+		System.out.println(str1.substring(1,len-1));
+	}
+
+}
