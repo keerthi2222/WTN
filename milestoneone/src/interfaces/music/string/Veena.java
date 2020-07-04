bd20894020e7794c21244c54cf949c61ccb43277
lenovo <lenovo@DESKTOP-NN19MM0>
2020-07-04 16:34:34 +0530

    Music

---
+++ milestoneone/src/interfaces/music/string/Veena.java
@@ -0,0 +1,11 @@
+package interfaces.music.string;
+import interfaces.music.*;
+
+public class Veena implements Playable
+{
+	public void play()
+	{
+		System.out.println("Playing Veena");
+	}
+
+}
---
+++ milestoneone/src/interfaces/music/wind/Saxophone.java
@@ -0,0 +1,11 @@
+package interfaces.music.wind;
+import interfaces.music.*;
+
+public class Saxophone implements Playable
+{
+	public void play()
+	{
+		System.out.println("Playing Saxophone");
+	}
+
+}
